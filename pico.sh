79169a08b22021775f056f75b8de604c3cf77b6b
Eric Bower <me@erock.io>
2026-06-12 22:06:41 -0400

    chore(ci): upload static site on main

--- pico.sh
+++ pico.sh
@@ -2,23 +2,34 @@
 set -euo pipefail
 
 export ZMX_SESSION_PREFIX="${ZMX_SESSION_PREFIX:-ci.zmx.}"
-EVENT_TYPE="${PICO_CI_EVENT_TYPE:-manual}"
+EVENT="${PICI_EVENT:-manual}"
 
-echo "running ci event=${EVENT_TYPE} session=${ZMX_SESSION_PREFIX}"
+echo "running ci event=${EVENT} session=${ZMX_SESSION_PREFIX}"
 
 zmx run build docker build -t zig-zmx .
 zmx run fmt -d docker run --rm -t zig-zmx:latest zig fmt --check .
 zmx run test -d docker run --rm -t zig-zmx:latest zig build test
-zmx run integration -d docker run --rm -t zig-zmx:latest bats test/session.bats
+zmx run integration -d docker run --rm -t zig-zmx:latest bats test/*.bats
 zmx wait "*"
 
-if [[ $EVENT_TYPE != "git.tag" ]]; then
+zmx run upload-build docker build -t zmx-upload -f Dockerfile.upload .
+
+if [[ $PICI_BRANCH = "main" ]]; then
+  zmx run upload docker run --rm \
+    -v "$(pwd)/README.md:/app/README.md:ro" \
+    -v "$(pwd)/logo.png:/app/logo.png:ro" \
+    -v "$(pwd)/index.tmpl:/app/index.tmpl:ro" \
+    -v ~/.ssh:/root/.ssh:ro \
+    zmx-upload
+fi
+
+if [[ $EVENT != "git.tag" ]]; then
   echo "success!"
   exit 0
 fi
 
-TAG="${PICO_CI_TAG_NAME}"
-NEW_VERSION="${PICO_CI_TAG_NAME#v}"
+TAG="${PICI_TAG}"
+NEW_VERSION="${PICI_TAG#v}"
 
 zmx run semver sed -i "s/\.version = \"[^\"]*\"/.version = \"$NEW_VERSION\"/" build.zig.zon && cat build.zig.zon
 zmx run update-readme sed -i "s/zmx-[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*/zmx-$NEW_VERSION/g" README.md
@@ -26,14 +37,10 @@ zmx run build-release -d docker run --rm -t zig-zmx:latest zig build release
 zmx run brew -d bash gen-brew.sh "$NEW_VERSION"
 
 echo "distributing bins"
-zmx run upload-build docker build -t zmx-upload -f Dockerfile.upload .
 zmx run upload docker run --rm \
-  -v "$(pwd)/README.md:/app/README.md:ro" \
-  -v "$(pwd)/logo.png:/app/logo.png:ro" \
-  -v "$(pwd)/index.tmpl:/app/index.tmpl:ro" \
   -v "$(pwd)/zig-out/dist:/app/dist:ro" \
   -v ~/.ssh:/root/.ssh:ro \
-  zmx-upload
+  zmx-upload rsync -rv dist/ pgs.sh:/zmx/a
 zmx run gh-build docker build -t gh-release -f Dockerfile.release .
 zmx run gh docker run --rm \
   -v "$(pwd)/zig-out/dist":/dist \
